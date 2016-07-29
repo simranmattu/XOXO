c66855563c7459736084643782f2db07b024133f
simranmattu <simran.mattu27@gmail.com>
2016-07-29 14:17:23 +0100

    SSSSS

--- jdxa.js
+++ jdxa.js
@@ -3,74 +3,74 @@
 
 var player_one = 1;
 //-------------------------------------------------------------------------------------------------------------------------
-
-function display_input(square){
-    if ( player_one == 1 ){
-        if (document.getElementById(square).innerHTML == "X" ||document.getElementById(square).innerHTML == "O"){
-            window.alert("Try another square");
-            document.getElementById(square).innerHTML = "X";
-
-
-
-            }
-           else{
-
-            fetchX(square);
-
-            player_one = 0;
-            checkWinner("case1","case2","case3")
-            checkWinner("case4","case5","case6")
-            checkWinner("case7","case8","case9")
-            checkWinner("case1","case4","case7")
-            checkWinner("case2","case5","case8")
-            checkWinner("case3","case6","case9")
-            checkWinner("case1","case5","case9")
-            checkWinner("case3","case5","case7")
-
-
-
-            }
-    }
-
-
-    else {
-        if (checkO(square)||checkX(square) ){
-             window.alert("Try another square");
-
-
-                    }
-        else{
-
-            fetchO(square);
-
-
-            player_one = 1;
-            checkWinner("case1","case2","case3")
-            checkWinner("case4","case5","case6")
-            checkWinner("case7","case8","case9")
-            checkWinner("case1","case4","case7")
-            checkWinner("case2","case5","case8")
-            checkWinner("case3","case6","case9")
-            checkWinner("case1","case5","case9")
-            checkWinner("case3","case5","case7")
-
-
-
-
-            //var  champion1 = checkWinner("case1","case2","case3");
-            //var champion2 = checkWinner("case4","case5","case6");
-            //var champion3 = checkWinner("case7","case8","case9");
+//
+//function display_input(square){
+//    if ( player_one == 1 ){
+//        if (document.getElementById(square).innerHTML == "X" ||document.getElementById(square).innerHTML == "O"){
+//            window.alert("Try another square");
+//            document.getElementById(square).innerHTML = "X";
+//
+//
+//
+//            }
+//           else{
+//
+//            fetchX(square);
+//
+//            player_one = 0;
+//            checkWinner("case1","case2","case3")
+//            checkWinner("case4","case5","case6")
+//            checkWinner("case7","case8","case9")
+//            checkWinner("case1","case4","case7")
+//            checkWinner("case2","case5","case8")
+//            checkWinner("case3","case6","case9")
+//            checkWinner("case1","case5","case9")
+//            checkWinner("case3","case5","case7")
+//
+//
+//
+//            }
+//    }
+//
+//
+//    else {
+//        if (checkO(square)||checkX(square) ){
+//             window.alert("Try another square");
+//
+//
+//                    }
+//        else{
+//
+//            fetchO(square);
+//
+//
+//            player_one = 1;
+//            checkWinner("case1","case2","case3")
+//            checkWinner("case4","case5","case6")
+//            checkWinner("case7","case8","case9")
+//            checkWinner("case1","case4","case7")
+//            checkWinner("case2","case5","case8")
+//            checkWinner("case3","case6","case9")
+//            checkWinner("case1","case5","case9")
+//            checkWinner("case3","case5","case7")
+//
+//
+//
+//
+//            //var  champion1 = checkWinner("case1","case2","case3");
+//            //var champion2 = checkWinner("case4","case5","case6");
+//            //var champion3 = checkWinner("case7","case8","case9");
 
     //var overall_champ = champion1||champion2||champion3
 
 //            var a, b, c;
 //            c = 1;
 //            var d = a || b || c;
-                    }
-            }
-
-
-}
+//                    }
+//            }
+//
+//
+//}
 
 //--------------------------------WINNER CODE--------------------------------------------------------------------------
 var winner ;
@@ -104,20 +104,69 @@ function checkO(location){
 
     }
 
-<<<<<<< HEAD
 //---------------------------------------------------------------------------------------------------------------------------
 
 
-function Grid() {
+//function Grid() {
+//    this.gridSetup = ko.observableArray([
+//      ko.observableArray(["X", "", "O"]),
+//      ko.observableArray(["X", "O", ""]),
+//      ko.observableArray(["O", "", ""])
+//     ]);
+//     }
+
+
+
+function MyViewModel(){
+
     this.gridSetup = ko.observableArray([
-      ko.observableArray(["X", "", "O"]),
-      ko.observableArray(["X", "O", ""]),
-      ko.observableArray(["O", "", ""])
-     ]);
-     }
+    ko.observableArray([new MyViewModel_Cell(),new MyViewModel_Cell(),new MyViewModel_Cell()]),
+    ko.observableArray([new MyViewModel_Cell(),new MyViewModel_Cell() ,new MyViewModel_Cell() ]),
+    ko.observableArray([new MyViewModel_Cell(), new MyViewModel_Cell(), new MyViewModel_Cell()])
+                ]);
 
-window.grid = new Grid();
 
+    }
+
+// //function display_input(square){
+   //    if ( player_one == 1 ){
+   //        if (document.getElementById(square).innerHTML == "X" ||document.getElementById(square).innerHTML == "O"){
+   //            window.alert("Try another square");
+   //            document.getElementById(square).innerHTML = "X";
+
+function MyViewModel_Cell(){
+    var self = this;
+    this.updateGrid = function(location){
+        this.userEntry = ko.observable('')
+    //console.log('B')
+        if (player_one == 1){
+            if (self.userEntry() == "X" || self.userEntry() == "O") {
+                window.alert("Try another square!");
+
+                }
+            else {
+                self.userEntry('O');
+                player_one=0
+
+            }
+        else {
+            if(self.userEntry() == "X" || self.userEntry() == "O") {
+                    window.alert("Try another square!");
+                 }
+            else {
+                    self.userEntry('X');
+                    player_one=1
+                    }
+
+
+
+
+        ///console.log('A')
+        console.log(self.userEntry())
+    }
+    }
+    }
+window.grid = new MyViewModel();
 //function convertToObservable(gridSetup)
 //{
 //    var newList = [];
@@ -130,23 +179,10 @@ window.grid = new Grid();
 //    });
 //    return newList;
 //}
+
+
 //
 //function updateGridX(){
-=======
-// function Grid() {
-// this.gridSetup = ko.observableArray([
- // new row (null, null, null )
-//   new row (null, null, null)
-//   new row (null, null, null)
-//    ])};
+ko.applyBindings(new MyViewModel());
 
-//ko.appkybindings (new Grid());
->>>>>>> origin/master
-
-//------------------------------------------------------------------------------------
-
-<<<<<<< HEAD
-ko.applyBindings(window.grid);
-=======
-var Mut = new Array
->>>>>>> origin/master
+// works ------ko.applyBindings(new window.grid);
